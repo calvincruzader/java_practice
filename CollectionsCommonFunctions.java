d503d3d6c87ad4d8426f6e82a2f19988594bf53c
Calvin <calvincruzader@gmail.com>
2018-12-29 13:27:30 -0600

    Adds some discoveries about collections/hashmaps/linked lists/etc.

--- CollectionsCommonFunctions.java
+++ CollectionsCommonFunctions.java
@@ -1,6 +1,33 @@
+import java.util.*;
+
 class CollectionsCommonFunctions {
 	
 	public static void main(String[] args) { 
+		// collectionsHashMaps();
+		collectionsLL();
+	}
 
+	public static void collectionsHashMaps() { 
+		HashMap<String, String> hm = new HashMap<String, String>();
+		// hm.fill("testing");
+
+		Class c = hm.getClass();
+		while(c != null) {
+			System.out.println(c.getName());
+			c = c.getSuperclass();
+		}
+
+		// there is no bare bones hash iterator
+		Iterator<String> it = hm.keySet().iterator();
+		while(it.hasNext()) System.out.println(hm.get(it));
+	}
+
+	public static void collectionsLL() { 
+		LinkedList<String> ll = new LinkedList<String>();
+		Class c = ll.getClass();
+		while(c != null) {
+			System.out.println(c.getName());
+			c = c.getSuperclass();
+		}
 	}
 }
